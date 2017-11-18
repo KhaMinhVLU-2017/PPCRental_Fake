8c5f2c57dbf9a3566c8725af372b4301942f56d0
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2017-11-18 11:18:47 +0700

    Begin Route Area

---
+++ demo_PPCRental/Areas/Area/Models/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using demo_PPCRental.Models;
+
+namespace demo_PPCRental.Areas.Area.Models
+{
+    public class LoginController : Controller
+    {
+        DemoPPCRentalEntities db = new DemoPPCRentalEntities();
+        // GET: Area/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string txt_user,string txt_pass)
+        {
+            USER meo = db.USERs.FirstOrDefault(s => s.Email == txt_user && s.Password == txt_pass);
+            if (meo==null)
+            {
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+          
+        }
+    }
+}
---
+++ demo_PPCRental/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using demo_PPCRental.Models;
+using System.IO;
+
+namespace demo_PPCRental.Controllers
+{
+    public class HomeController : Controller
+    {
+        DemoPPCRentalEntities db = new DemoPPCRentalEntities();
+        public ActionResult Index()
+        {
+            List<PROPERTY> meo = new List<PROPERTY>();
+            meo = db.PROPERTies.ToList();
+            return View(meo);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Search(string meo)
+        {
+            var hey = db.PROPERTies.ToList().Where(s => s.PropertyName.ToLower().Contains(meo) || s.Content.ToLower().Contains(meo)
+                ||s.DISTRICT.DistrictName.ToLower().Contains(meo));
+            return View(hey);
+        }
+        public ActionResult Detail(int? id)
+        {
+            if (id != null)
+            {
+                PROPERTY meo = new PROPERTY();
+                meo = db.PROPERTies.Find(id);
+                return View(meo);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            var path = "";
+            if (file != null)
+            {
+                if (file.ContentLength > 0)
+                {
+                    if (Path.GetExtension(file.FileName).ToLower() == ".jpg"||
+                            Path.GetExtension(file.FileName).ToLower()==".png"||
+                            Path.GetExtension(file.FileName).ToLower()==".gif"||
+                            Path.GetExtension(file.FileName).ToLower()==".jpeg")
+                    {
+                        path = Path.Combine(Server.MapPath("~/Images/"), file.FileName);
+                        file.SaveAs(path);
+                     
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
